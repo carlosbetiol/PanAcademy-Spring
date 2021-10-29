eb0f3eb590e96c30f03c4a52b9b26ce1dbbb93e3
Carlos Betiol <carlos@sistelpa.com>
2021-10-28 22:50:31 -0300

    implementado endpoints delete, post e put

--- SpringAula2/src/main/java/dev/betiol/springaula2/controllers/UsuarioController.java
+++ SpringAula2/src/main/java/dev/betiol/springaula2/controllers/UsuarioController.java
@@ -4,11 +4,10 @@ import dev.betiol.springaula2.model.Usuario;
 import dev.betiol.springaula2.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +27,27 @@ public class UsuarioController {
     public ResponseEntity<List<Usuario>> findAll() {
         List<Usuario> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Usuario> update(@PathVariable Integer id, @RequestBody
+            Usuario obj) {
+        Usuario newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario obj){
+        Usuario newObj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- SpringAula2/src/main/java/dev/betiol/springaula2/service/UsuarioService.java
+++ SpringAula2/src/main/java/dev/betiol/springaula2/service/UsuarioService.java
@@ -19,6 +19,23 @@ public class UsuarioService {
         return obj.orElse(null);
     }
 
+    public Usuario update(Integer id, Usuario obj) {
+        Usuario newObj = findById(id);
+        newObj.setNome(obj.getNome());
+        newObj.setSenha(obj.getSenha());
+        return repository.save(newObj);
+    }
+
+    public void delete(Integer id) {
+        findById(id);
+        repository.deleteById(id);
+    }
+
+    public Usuario create(Usuario obj) {
+        obj.setId(null);
+        return repository.save(obj);
+    }
+
     public List<Usuario> findAll() {
         return repository.findAll();
     }
