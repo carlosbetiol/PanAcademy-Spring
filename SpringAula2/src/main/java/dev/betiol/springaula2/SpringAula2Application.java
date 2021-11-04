71302eb0d87d9ae8461542936cb18a35e74f836c
Carlos Betiol <carlos@sistelpa.com>
2021-11-04 20:57:41 -0300

    adicionado projeto de exemplo com mongodb e atividade consultorio utilizando mongodb

--- SpringAula2/src/main/java/dev/betiol/springaula2/SpringAula2Application.java
+++ SpringAula2/src/main/java/dev/betiol/springaula2/SpringAula2Application.java
@@ -12,28 +12,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class SpringAula2Application implements CommandLineRunner {
+//public class SpringAula2Application implements CommandLineRunner {
+public class SpringAula2Application {
 
-    @Autowired
-    UsuarioRepository usuarioRepository;
-
-    @Autowired
-    ProdutoRepository produtoRepository;
+//    @Autowired
+//    UsuarioRepository usuarioRepository;
+//
+//    @Autowired
+//    ProdutoRepository produtoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAula2Application.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-        Usuario u1 = new Usuario("Maria Silva", "1234");
-        Usuario u2 = new Usuario("Joao da Silva", "1234");
-        usuarioRepository.saveAll(Arrays.asList(u1, u2));
-
-        Produto p1 = new Produto("Impressora Epson lx300");
-        Produto p2 = new Produto("Monitor Dell 2564");
-        produtoRepository.saveAll(Arrays.asList(p1, p2));
-
-    }
+    //    @Override
+//    public void run(String... args) throws Exception {
+//
+//        Usuario u1 = new Usuario("Maria Silva", "1234");
+//        Usuario u2 = new Usuario("Joao da Silva", "1234");
+//        usuarioRepository.saveAll(Arrays.asList(u1, u2));
+//
+//        Produto p1 = new Produto("Impressora Epson lx300");
+//        Produto p2 = new Produto("Monitor Dell 2564");
+//        produtoRepository.saveAll(Arrays.asList(p1, p2));
+//
+//    }
 }
---
+++ SpringAula2/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into usuario (nome,senha) values ('Maria Silva', '1234');
+insert into usuario (nome,senha) values ('Joao da Silva', '1234');
+
+insert into produto (nome) values ('Impressora Epson lx300');
+insert into produto (nome) values ('Monitor Dell 2564');
